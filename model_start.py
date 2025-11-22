4893b8b66eb1dc7d5f240c422c7628388f7bac49
w臻 <61671162+wz043@users.noreply.github.com>
2025-11-22 22:50:33 +0800

    Update model_start.py

--- model_start.py
+++ model_start.py
@@ -4,7 +4,7 @@ from model.inference_model import GMT_Zero, ModelArgs
 import transformers
 import lightning as L
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-class GMT_1(L.LightningModule):
+class GMT_Zero_L(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = GMT_Zero(ModelArgs())
@@ -12,12 +12,12 @@ class GMT_1(L.LightningModule):
     def forward(self,x):
         return self.model(x)
 
-model = GMT_1.load_from_checkpoint(checkpoint_path="./model_weight/post_train_weight.ckpt",strict=False).to(device)
+model = GMT_Zero_L.load_from_checkpoint(checkpoint_path="./model_weight/GMT_Zero.ckpt",strict=False).to(device)
 tokenizer = transformers.AutoTokenizer.from_pretrained("./tokenizer/tokenizer")
 
-eos_token_id = torch.tensor([tokenizer.eos_token_id], device='cuda')
+eos_token_id = torch.tensor([tokenizer.eos_token_id], device=device)
 @torch.inference_mode()
-def generate(tokenizer,input_ids,T,P,max_len:int=30):
+def generate(tokenizer,input_ids,T,P,max_len:int=100):
     current_sequence = input_ids.clone()
     for i in range(max_len):
         logits = model(input_ids)[:,-1,:] / T
@@ -29,16 +29,15 @@ def generate(tokenizer,input_ids,T,P,max_len:int=30):
         sorted_probs[~nucleus] = 0
         sorted_probs /= sorted_probs.sum()
         select_ids = torch.multinomial(sorted_probs,num_samples=1)
-        predict_ids = indices.gather(-1, select_ids)
-        input_ids = torch.cat([current_sequence, predict_ids], dim=-1)
-        current_sequence = input_ids
+        input_ids = indices.gather(-1, select_ids)
+        current_sequence = torch.cat([current_sequence, input_ids], dim=-1)
 
-        if predict_ids == tokenizer.eos_token_id:
+        if input_ids == tokenizer.eos_token_id:
             break
 
-    return tokenizer.decode(current_sequence[-1], skip_special_tokens=True)
+    return tokenizer.decode(current_sequence[0], skip_special_tokens=True)
 
 while True:
     inference_text = input("enter your question:",)
-    input_ids = tokenizer.encode(inference_text, return_tensors="pt",add_special_tokens=True).to('cuda')
+    input_ids = tokenizer.encode(inference_text, return_tensors="pt",add_special_tokens=True).to(device)
     print(generate(tokenizer, input_ids, 0.6,0.95))
